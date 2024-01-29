047571efe33b1957d8ea637218a773ab413c5667
Hussain-shahg <m.hussain103shah@gmail.com>
2024-01-29 17:35:46 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# praso_web_app - Flutter
+## website https://praso.com.br/
+
+A full online shopping website is working perfectly in Flutter application. I used the famous webview plugin for this purpose. I just did some customizations as my client required. This is a full perfect online mart based app. you can check the website by clicking on https://praso.com.br/ and also you can also see the GIF given below that show how this web looks in app side. A customer can also redirect to owner's whatsapp to ask any qestion.
+
+✔️ Sound NullSafety <br />
+✔️ Flutter WebView <br />
+
+## Architechture
+✔️ Clean Architechture <br />
+
+
+## Screenshots of web
+## 1
+<p align="center">
+  <img 
+    width=80%
+    height=80%
+    src="https://user-images.githubusercontent.com/101565812/206759340-96250378-8bcb-4a03-a28e-117599142110.png">
+</p>
+## 2
+<p align="center">
+  <img 
+    width=80%
+    height=80%
+    src="https://user-images.githubusercontent.com/101565812/206759352-858101b7-cb1c-4eef-92b9-8e02064c127f.png">
+</p>
+## 3
+<p align="center">
+  <img 
+    width=80%
+    height=80%
+    src="https://user-images.githubusercontent.com/101565812/206759357-18587ab5-8b72-467d-98ab-9f73e3fae657.png">
+</p>
+## 4
+<p align="center">
+  <img 
+    width=80%
+    height=80%
+    src="https://user-images.githubusercontent.com/101565812/206759364-52031e57-5ff3-4603-a1d3-f24c2bb52d3d.png">
+</p>
+
+## Screenshots of mobile app
+## 1
+<p align="center">
+  <img 
+    width=40%
+    height=40%
+    src="https://user-images.githubusercontent.com/101565812/206773584-46c9e65f-9326-45ce-ab35-9f9a7ca555ca.jpg">
+</p>
+
+## 2
+<p align="center">
+  <img 
+    width=40%
+    height=40%
+    src="https://user-images.githubusercontent.com/101565812/206773598-275b743b-57c7-4e66-ac75-8182a03fb1d0.jpg">
+</p>
+
+## 3
+<p align="center">
+  <img 
+    width=40%
+    height=40%
+    src="https://user-images.githubusercontent.com/101565812/206773610-09ecc131-9f6e-4a0a-ab9b-73079f4e64f6.jpg">
+</p>
+
+## 4
+<p align="center">
+  <img 
+    width=40%
+    height=40%
+    src="https://user-images.githubusercontent.com/101565812/206773617-4de6c46b-d70b-4ea6-a88e-1bd6829552d9.jpg">
+</p>
+
+## 5
+<p align="center">
+  <img 
+    width=40%
+    height=40%
+    src="https://user-images.githubusercontent.com/101565812/206773628-65e84eb8-b898-43e7-b0e4-851df1e48a93.jpg">
+</p>
+
+## GIF
+
+<p align="center">
+  <img 
+    width=40%
+    height=40%
+    src="https://user-images.githubusercontent.com/101565812/206776711-1f0c51df-63ec-43f7-81e3-f74ec3f0487c.gif">
+</p>
+
+
+
+
+
+
+
---
+++ lib/web_view_redirect.dart
@@ -0,0 +1,305 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:isolate';
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+final key = UniqueKey();
+
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({Key? key}) : super(key: key);
+
+  @override
+  _WebViewPageState createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  WebViewController? _webViewController;
+  bool whatsAppVisibility = true;
+  ReceivePort _port = ReceivePort();
+  Timer? timer;
+  PrasoNotifyProvider? prasoNotifyProvider;
+
+  final Completer<WebViewController> _controllerCompleter =
+      Completer<WebViewController>();
+
+  bool isExit = false;
+
+  openWhatsApp() async {
+    String whatsapp = '+55 81 96974-371';
+    String message =
+        'Olá, Pedro! Tudo bem? Tenho uma dúvida sobre a Praso, você poderia me ajudar?';
+    String whatsappURlAndroid =
+        "whatsapp://send?phone=" + whatsapp + "&text=$message";
+    String whatsappURLIos =
+        "https://wa.me/$whatsapp?text=${Uri.parse(message)}";
+    if (Platform.isIOS) {
+      // for iOS phone only
+      if (await canLaunch(whatsappURLIos)) {
+        await launch(whatsappURLIos, forceSafariVC: false);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("WhatsApp não está instalado.")));
+      }
+    } else {
+      // android , web
+      if (await canLaunch(whatsappURlAndroid)) {
+        await launch(whatsappURlAndroid);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("WhatsApp não está instalado")));
+      }
+    }
+  }
+
+  Widget bottomNavigationBarItems(
+      {required String imagePath,
+      required String iconText,
+      required Color color}) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image(height: 30, width: 30, image: AssetImage(imagePath)),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 4.0, top: 4.0),
+          child: Text(
+            iconText,
+            style: TextStyle(color: color),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void downloadFile({required String url}) async{
+
+    final taskId = await FlutterDownloader.enqueue(
+      saveInPublicStorage: true,
+      fileName: 'prasoInvoice_${DateTime.now().toString().replaceAll(' ', '').replaceAll(':', '').replaceAll('-', '').replaceAll('.', '')}',
+      url: url,
+      savedDir: '/storage/emulated/0/',
+      showNotification: true, // show download progress in status bar (for Android)
+      openFileFromNotification: true, // click on notification to open downloaded file (for Android)
+    );
+    final tasks = await FlutterDownloader.loadTasks();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    prasoNotifyProvider = Provider.of(context, listen: false);
+    WebView.platform = SurfaceAndroidWebView();
+
+    IsolateNameServer.registerPortWithName(_port.sendPort, 'downloader_send_port');
+    _port.listen((dynamic data) {
+      String id = data[0];
+      DownloadTaskStatus status = data[1];
+      int progress = data[2];
+      setState((){
+
+      });
+    });
+
+    FlutterDownloader.registerCallback(downloadCallback);
+  }
+
+  @override
+  void dispose() {
+    IsolateNameServer.removePortNameMapping('downloader_send_port');
+    super.dispose();
+  }
+
+  static void downloadCallback(String id, DownloadTaskStatus status, int progress) {
+    final SendPort? send = IsolateNameServer.lookupPortByName('downloader_send_port');
+    send!.send([id, status, progress]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: WillPopScope(
+        onWillPop: () async {
+          if (await _webViewController!.canGoBack()) {
+            _webViewController!.goBack();
+            return Future.value(false);
+          } else {
+            showDialog(
+              barrierDismissible: false,
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('Confirmar saída?'),
+                  content: const Text('Tem certeza que deseja sair?'),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          isExit = false;
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Cancelar')),
+                    TextButton(
+                        onPressed: () {
+                          isExit = true;
+                          SystemNavigator.pop();
+                        },
+                        child: const Text('Saída')),
+                  ],
+                );
+              },
+            );
+            return Future.value(isExit);
+          }
+        },
+        child: Scaffold(
+          body: Stack(children: [
+            //webView
+            WebView(
+              initialUrl: 'https://praso.com.br/',
+              javascriptMode: JavascriptMode.unrestricted,
+              key: key,
+              onWebViewCreated: (WebViewController webViewController) {
+                _controllerCompleter.future
+                    .then((value) => _webViewController = value);
+                _controllerCompleter.complete(webViewController);
+              },
+              onPageStarted: (url) {
+                prasoNotifyProvider!.url = url;
+              },
+              navigationDelegate: (navigation) async {
+
+                return Future.value(NavigationDecision.navigate);
+                },
+              gestureNavigationEnabled: true,
+            ),
+
+            //bottom navigation bar
+            Positioned(
+              right: 0,
+              bottom: 0,
+              left: 0,
+              child: Card(
+                elevation: 20,
+                child: Container(
+                    height: 90,
+                    color: Colors.white,
+                    child: Selector<PrasoNotifyProvider, String>(
+                      selector: (context, prasoNotifyProvider) => prasoNotifyProvider.url,
+                      builder: (context, value, child) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          //home icon
+                          GestureDetector(
+                              onTap: () async {
+                                while (await _webViewController!.canGoBack()) {
+                                  _webViewController!.goBack();
+                                  prasoNotifyProvider!.homeIcon = Colors.blue;
+                                  prasoNotifyProvider!.categoryIcon =
+                                      Colors.black;
+                                  prasoNotifyProvider!.accountIcon =
+                                      Colors.black;
+                                }
+                              },
+                              child: Selector<PrasoNotifyProvider, Color>(
+                                  selector: (context, prasoNotifyProvider) =>
+                                      prasoNotifyProvider.homeIcon,
+                                  builder: (context, value, child) =>
+                                      bottomNavigationBarItems(
+                                          imagePath: prasoNotifyProvider!.url.length == 'https://praso.com.br/'.length ? 'assets/images/home_select.png':'assets/images/home_icon.png',
+                                          iconText: 'início',
+                                          color: prasoNotifyProvider!.homeIcon))),
+
+                          //category icon
+                          GestureDetector(
+                              onTap: () {
+                                prasoNotifyProvider!.homeIcon = Colors.black;
+                                prasoNotifyProvider!.categoryIcon = Colors.blue;
+                                prasoNotifyProvider!.accountIcon = Colors.black;
+                                _webViewController!.loadUrl('https://praso.com.br/collections/acucares');
+                              },
+                              child: Selector<PrasoNotifyProvider, Color>(
+                                  selector: (context, prasoNotifyProvider) =>
+                                      prasoNotifyProvider.categoryIcon,
+                                  builder: (context, value, child) =>
+                                      bottomNavigationBarItems(imagePath: prasoNotifyProvider!.url.length == 'https://praso.com.br/collections/acucares'.length ? 'assets/images/catagories_select.png': 'assets/images/categories_icon.png',
+                                          iconText: 'categorias',
+                                          color: prasoNotifyProvider!
+                                              .categoryIcon))),
+
+                          //account icon
+                          GestureDetector(
+                              onTap: () {
+                                prasoNotifyProvider!.homeIcon = Colors.black;
+                                prasoNotifyProvider!.categoryIcon =
+                                    Colors.black;
+                                prasoNotifyProvider!.accountIcon = Colors.blue;
+                                _webViewController!.loadUrl('https://praso.com.br/account/login?return_url=%2Faccount');
+                              },
+                              child: Selector<PrasoNotifyProvider, Color>(
+                                  selector: (context, prasoNotifyProvider) =>
+                                      prasoNotifyProvider.accountIcon,
+                                  builder: (context, value, child) =>
+                                      bottomNavigationBarItems(
+                                          imagePath: prasoNotifyProvider!.url.length == 'https://praso.com.br/account/login?return_url=%2Faccount'.length ? 'assets/images/contact_select.png' :'assets/images/contact_icon.png',
+                                          iconText: 'conta',
+                                          color: prasoNotifyProvider!
+                                              .accountIcon))),
+                          GestureDetector(
+                              onTap: () {
+                                openWhatsApp();
+                              },
+                              child: bottomNavigationBarItems(
+                                  imagePath: 'assets/images/whats_app_icon.png',
+                                  iconText: 'WhatsApp',
+                                  color: Colors.black)),
+                        ],
+                      ),
+                    )),
+              ),
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class PrasoNotifyProvider with ChangeNotifier {
+  String _url = '';
+  Color _homeIcon = Colors.blue,
+      _categoryIcon = Colors.black,
+      _accountIcon = Colors.black;
+
+  String get url => _url;
+
+  set url(String value) {
+    _url = value;
+    notifyListeners();
+  }
+
+  get accountIcon => _accountIcon;
+
+  set accountIcon(value) {
+    _accountIcon = value;
+    notifyListeners();
+  }
+
+  get categoryIcon => _categoryIcon;
+
+  set categoryIcon(value) {
+    _categoryIcon = value;
+    notifyListeners();
+  }
+
+  Color get homeIcon => _homeIcon;
+
+  set homeIcon(Color value) {
+    _homeIcon = value;
+    notifyListeners();
+  }
+}
